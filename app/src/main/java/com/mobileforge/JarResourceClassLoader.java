29f9483b51287b67569b4bb0c084daa3f258454b
thegrit42 <thegritshop42@gmail.com>
2026-01-10 05:00:13 -0800

    Fix ECJ resource bundle loading in JarResourceClassLoader
    - Cache ZipFile instance instead of creating new ones
    - Read resources into byte arrays to avoid stream closing issues
    - Properly delegate to parent classloader for resource lookup

--- app/src/main/java/com/mobileforge/JarResourceClassLoader.java
+++ app/src/main/java/com/mobileforge/JarResourceClassLoader.java
@@ -1,5 +1,6 @@
 package com.mobileforge;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
@@ -8,20 +9,35 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class JarResourceClassLoader extends ClassLoader {
-    private File jarFile;
+    private ZipFile zipFile;
 
     public JarResourceClassLoader(File jarFile, ClassLoader parent) {
         super(parent);
-        this.jarFile = jarFile;
+        try {
+            this.zipFile = new ZipFile(jarFile);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to open JAR file: " + jarFile, e);
+        }
     }
 
     @Override
     public InputStream getResourceAsStream(String name) {
         try {
-            ZipFile zip = new ZipFile(jarFile);
-            ZipEntry entry = zip.getEntry(name);
+            ZipEntry entry = zipFile.getEntry(name);
             if (entry != null) {
-                return zip.getInputStream(entry);
+                // Read entire stream into byte array to avoid ZipFile closing issues
+                InputStream in = zipFile.getInputStream(entry);
+                byte[] buffer = new byte[(int) entry.getSize()];
+                int offset = 0;
+                int remaining = buffer.length;
+                while (remaining > 0) {
+                    int read = in.read(buffer, offset, remaining);
+                    if (read < 0) break;
+                    offset += read;
+                    remaining -= read;
+                }
+                in.close();
+                return new ByteArrayInputStream(buffer);
             }
         } catch (Exception e) {
             // Fall through
@@ -31,22 +47,36 @@ public class JarResourceClassLoader extends ClassLoader {
 
     @Override
     public URL getResource(String name) {
+        // Check parent first
+        URL parentResource = super.getResource(name);
+        if (parentResource != null) {
+            return parentResource;
+        }
+
         try {
-            ZipFile zip = new ZipFile(jarFile);
-            ZipEntry entry = zip.getEntry(name);
+            ZipEntry entry = zipFile.getEntry(name);
             if (entry != null) {
-                // Return jar:file:// URL
-                return new URL("jar:file:" + jarFile.getAbsolutePath() + "!/" + name);
+                // Create a custom URL that our handler can understand
+                return new URL("jar:file:" + zipFile.getName() + "!/" + name);
             }
         } catch (Exception e) {
             // Fall through
         }
-        return super.getResource(name);
+        return null;
     }
 
     @Override
     public Enumeration<URL> getResources(String name) throws java.io.IOException {
-        // Just delegate to parent
         return super.getResources(name);
+    }
+
+    public void close() {
+        try {
+            if (zipFile != null) {
+                zipFile.close();
+            }
+        } catch (Exception e) {
+            // Ignore
+        }
     }
 }
